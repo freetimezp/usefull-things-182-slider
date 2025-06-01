e79a0ef594630fbc7df39990ab28bc2e3262e77f
Yevhen Onoprienko <fromzpcity@gmail.com>
2025-06-02 00:43:08 +0300

    03. usefull-things-182-slider. GSAP slider animation.

---
+++ src/assets/js/slides.js
@@ -0,0 +1,35 @@
+import img1 from "/src/assets/images/img1.jpg";
+import img2 from "/src/assets/images/img2.jpg";
+import img3 from "/src/assets/images/img3.jpg";
+import img4 from "/src/assets/images/img4.jpg";
+import img5 from "/src/assets/images/img5.jpg";
+
+const slides = [
+    {
+        title: "Wings's Night",
+        image: img1,
+        url: "https://www.google.com",
+    },
+    {
+        title: "Bold Dreams",
+        image: img2,
+        url: "https://www.google.com",
+    },
+    {
+        title: "Shards's Flowers",
+        image: img3,
+        url: "https://www.google.com",
+    },
+    {
+        title: "Silk in the Pirates",
+        image: img4,
+        url: "https://www.google.com",
+    },
+    {
+        title: "Growing Ashes",
+        image: img5,
+        url: "https://www.google.com",
+    },
+];
+
+export default slides;
